0d47bbd308c64e9761d955ad2950c20a2cb1ee68
Mike <micheld@gmail.com>
2018-12-05 21:14:33 -0500

    init

---
+++ AoC2018.tests/Inputs.cs
@@ -0,0 +1,45 @@
+﻿namespace AoC2018.tests
+{
+    public partial class Inputs
+    {
+        public static readonly string Day1 = @"";
+        public static readonly string Day2 = @"";
+        public static readonly string Day3 = @"";
+        public static readonly string Day4 = @"";
+        public static readonly string Day5 = @"";
+        public static readonly string Day6 = @"";
+        public static readonly string Day7 = @"";
+        public static readonly string Day8 = @"";
+        public static readonly string Day9 = @"";
+        public static readonly string Day10 = @"";
+        public static readonly string Day11 = @"";
+        public static readonly string Day12 = @"";
+        public static readonly string Day13 = @"";
+        public static readonly string Day14 = @"";
+        public static readonly string Day15 = @"";
+        public static readonly string Day16 = @"";
+        public static readonly string Day17 = @"";
+        public static readonly string Day18 = @"";
+        public static readonly string Day19 = @"";
+        public static readonly string Day20 = @"";
+        public static readonly string Day21 = @"";
+        public static readonly string Day22 = @"";
+        public static readonly string Day23 = @"";
+        public static readonly string Day24 = @"";
+        public static readonly string Day25 = @"";
+
+        /* to set the fields and keep them out of source control, declare a new part for the Inputs class and set the values in the static constructor : */
+        /* and then "ignore" the file in git */
+        /* public partial class Inputs 
+         * {
+         *     static Inputs()
+         *     {
+         *          Day1 = @"...";
+         *          Day2 = @"...";
+         *              ...
+         *          Day25 = @"...";
+         *     }
+         * }
+         */
+    }
+}
