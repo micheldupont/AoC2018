2d5c90113436482de7f77b40d15c0de1bfb10277
Michel Dupont <micheld@gmail.com>
2018-12-07 11:42:30 -0500

    day 7 - Q1

---
+++ AoC2018.tests/Day7Tests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace AoC2018.tests
+{
+	public class Day7Tests
+	{
+		private readonly string _testInput = @"Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.";
+
+		[Fact]
+		public void CanParseSteps()
+		{
+			var inputs = _testInput.Split(new[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+
+			var results = Day7.Parse(inputs);
+
+			Assert.Equal(6, results.Count());
+		}
+
+		[Fact]
+		public void CanParseStepsAndReturnThemInAlphaOrder()
+		{
+			var inputs = _testInput.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+			var results = Day7.Parse(inputs);
+
+			Assert.Equal("A", results[0].Name);
+			Assert.Equal("F", results[5].Name);
+		}
+
+		[Fact]
+		public void CanParseStepsWithUnlockList()
+		{
+			var inputs = _testInput.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+			var results = Day7.Parse(inputs);
+
+			Assert.Equal(2, results[0].Unlocks.Count());
+			Assert.Equal(1, results[1].Unlocks.Count());
+			Assert.Equal(2, results[2].Unlocks.Count());
+			Assert.Equal(1, results[3].Unlocks.Count());
+			Assert.Equal(0, results[4].Unlocks.Count());
+			Assert.Equal(1, results[5].Unlocks.Count());
+		}
+
+		[Fact]
+		public void CanFlagStepsWithoutDependencies()
+		{
+			var inputs = _testInput.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+			var results = Day7.Parse(inputs);
+
+			Day7.UpdateStatus(results);
+
+			Assert.True(results[0].IsLocked);
+			Assert.True(results[1].IsLocked);
+			Assert.False(results[2].IsLocked);
+			Assert.True(results[3].IsLocked);
+			Assert.True(results[4].IsLocked);
+			Assert.True(results[5].IsLocked);
+		}
+
+		[Fact]
+		public void CanReturnUnlockedSingleStep()
+		{
+			var inputs = _testInput.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+			var steps = Day7.Parse(inputs);
+
+			var results = Day7.FindUnlockedSteps(steps);
+
+			Assert.Equal(1, results.Count());
+			Assert.Equal("C", results[0].Name);
+		}
+
+		[Fact]
+		public void CanReturnUnlockedSteps()
+		{
+			var inputs = _testInput.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+			var steps = Day7.Parse(inputs);
+			steps.RemoveAt(2);
+
+			var results = Day7.FindUnlockedSteps(steps);
+
+			Assert.Equal(2, results.Count());
+			Assert.Equal("A", results[0].Name);
+			Assert.Equal("F", results[1].Name);
+		}
+
+		[Fact]
+		public void CanFindTheRightUnlockSequence()
+		{
+			var inputs = _testInput.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+			var steps = Day7.Parse(inputs);
+
+			var result = Day7.GetUnlockSequence(steps);
+
+			Assert.Equal("CABDFE", result);
+		}
+
+		[Fact]
+		public void Q1()
+		{
+			var inputs = Inputs.Day7.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+			var steps = Day7.Parse(inputs);
+
+			var result = Day7.GetUnlockSequence(steps);
+
+			Assert.Equal("GJKLDFNPTMQXIYHUVREOZSAWCB", result);
+		}
+	}
+}
---
+++ AoC2018/Day7.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2018
+{
+	public class Day7
+	{
+		/* Step C must be finished before step A can begin. */
+		/*   0  1   2   3     4      5      6  7  8    9    */
+		public static List<Step> Parse(string[] inputs)
+		{
+			var list = new List<Step>();
+
+			foreach (var line in inputs)
+			{
+				var parts = line.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+				var name = parts[1];
+				var unlockName = parts[7];
+
+				var step = list.Find(s => s.Name == name);
+				if (step == null)
+				{
+					step = new Step{Name = name};
+					list.Add(step);
+				}
+
+				var unlock = list.Find(s => s.Name == unlockName);
+				if (unlock == null)
+				{
+					unlock = new Step { Name = unlockName };
+					list.Add(unlock);
+				}
+
+				step.Unlocks.Add(unlock);
+			}
+
+			return list.OrderBy(s => s.Name).ToList();
+		}
+
+		public static void UpdateStatus(List<Step> steps)
+		{
+			var allUnlocks = steps.SelectMany(s => s.Unlocks).ToList();
+
+			foreach (var step in steps)
+			{
+				step.IsLocked = true;
+				if (allUnlocks.All(u => u.Name != step.Name))
+				{
+					step.IsLocked = false;
+				}
+			}
+		}
+
+		public static List<Step> FindUnlockedSteps(List<Step> steps)
+		{
+			UpdateStatus(steps);
+
+			return steps.Where(s => !s.IsLocked).OrderBy(s => s.Name).ToList();
+		}
+
+		public static string GetUnlockSequence(List<Step> steps)
+		{
+			var result = "";
+			while (steps.Count > 0)
+			{
+				var unlocked = FindUnlockedSteps(steps);
+
+				var firstUnlocked = unlocked.First();
+				result += firstUnlocked.Name;
+
+				steps.Remove(firstUnlocked);
+			}
+
+			return result;
+		}
+	}
+
+	public class Step
+	{
+		public string Name { get; set; }
+		public List<Step> Unlocks { get; }
+		public bool IsLocked { get; set; }
+
+		public Step()
+		{
+			Unlocks = new List<Step>();
+			IsLocked = true;
+		}
+	}
+}
+ 
