f4da8b3a401e6028233ec4438c5a6d8ae14ce456
Mike <micheld@gmail.com>
2018-12-08 14:52:42 -0500

    Day8

---
+++ AoC2018.tests/Day8Tests.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+
+namespace AoC2018.tests
+{
+    public class Day8Tests
+    {
+        private readonly string _exampleInput = @"2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
+
+        [Fact]
+        public void CanParseInts()
+        {
+            var day8 = new Day8(_exampleInput);
+
+            Assert.Equal(16, day8.InputLength);
+        }
+
+        [Fact]
+        public void CanBuildFirstNodeHeader()
+        {
+            var result = new Day8(_exampleInput).ParseNodes();
+
+            Assert.Equal(2, result.ChildCount);
+            Assert.Equal(3, result.MetadataLength);
+        }
+
+        [Fact]
+        public void CanBuildFirstNodeWithChildsAndMetadata()
+        {
+            var result = new Day8(_exampleInput).ParseNodes();
+
+            Assert.Equal(2, result.ChildNodes.Count);
+            Assert.Equal(3, result.Metadata.Count);
+        }
+
+        [Fact]
+        public void CanCalculateChecksum()
+        {
+            var root = new Day8(_exampleInput).ParseNodes();
+
+            Assert.Equal(138, root.GetChecksum());
+        }
+
+        [Fact]
+        public void Q1()
+        {
+            var root = new Day8(Inputs.Day8).ParseNodes();
+
+            Assert.Equal(46096, root.GetChecksum());
+        }
+
+        [Fact]
+        public void CanCalculateValue()
+        {
+            var root = new Day8(_exampleInput).ParseNodes();
+
+            Assert.Equal(66, root.GetValue());
+        }
+
+        [Fact]
+        public void Q2()
+        {
+            var root = new Day8(Inputs.Day8).ParseNodes();
+
+            Assert.Equal(24820, root.GetValue());
+        }
+    }
+}
---
+++ AoC2018/Day8.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2018
+{
+    public class Day8
+    {
+        private readonly List<int> _inputs;
+
+        public Day8(string input)
+        {
+            var parts = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            _inputs = parts.Select(int.Parse).ToList();
+        }
+
+        public int InputLength => _inputs.Count;
+
+        public Node ParseNodes()
+        {
+            var root = new Node();
+            ParseNodeAt(0, root);
+
+            return root;
+        }
+
+        private int ParseNodeAt(int index, Node thisNode)
+        {
+            var currentIndex = index;
+
+            thisNode.ChildCount = _inputs[currentIndex];
+            currentIndex++;
+
+            thisNode.MetadataLength = _inputs[currentIndex];
+            currentIndex++;
+ 
+            for (var i = 0; i < thisNode.ChildCount; i++)
+            {            
+                var child = new Node();
+                thisNode.ChildNodes.Add(child);
+                currentIndex = ParseNodeAt(currentIndex, child);
+            }
+
+            for (var i = 0; i < thisNode.MetadataLength; i++)
+            {
+                thisNode.Metadata.Add(_inputs[currentIndex]);
+                currentIndex++;
+            }
+
+            return currentIndex;
+        }
+    }
+
+    public class Node
+    {
+        public int ChildCount { get; set; }
+        public int MetadataLength { get; set; }
+
+        public List<Node> ChildNodes { get; }
+        public List<int> Metadata { get;}
+
+        public Node()
+        {
+            ChildCount = 0;
+            ChildNodes = new List<Node>();
+
+            MetadataLength = 0;
+            Metadata = new List<int>();
+        }
+
+        public int GetChecksum()
+        {
+            var checksum = Metadata.Sum();
+
+            foreach (var childNode in ChildNodes)
+            {
+                checksum += childNode.GetChecksum();
+            }
+
+            return checksum;
+        }
+
+        public int GetValue()
+        {
+            var value = 0;
+
+            if (ChildCount == 0)
+            {
+                return Metadata.Sum();
+            }
+
+            foreach (var i in Metadata)
+            {
+                if(i == 0)
+                    continue;
+
+                var index = i - 1;
+                if(index >= ChildCount)
+                    continue;
+
+                value += ChildNodes[index].GetValue();
+            }
+
+            return value;
+        }
+    }
+}
