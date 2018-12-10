8f4c34036d557d3f5ad006d3391b7f8ad4c96279
Mike <micheld@gmail.com>
2018-12-10 10:26:46 -0500

    day 10

---
+++ AoC2018.tests/Day10Tests.cs
@@ -0,0 +1,152 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace AoC2018.tests
+{
+    public class Day10Tests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Day10Tests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        #region Test Input
+
+        private readonly string _testInput = @"position=< 9,  1> velocity=< 0,  2>
+position=< 7,  0> velocity=<-1,  0>
+position=< 3, -2> velocity=<-1,  1>
+position=< 6, 10> velocity=<-2, -1>
+position=< 2, -4> velocity=< 2,  2>
+position=<-6, 10> velocity=< 2, -2>
+position=< 1,  8> velocity=< 1, -1>
+position=< 1,  7> velocity=< 1,  0>
+position=<-3, 11> velocity=< 1, -2>
+position=< 7,  6> velocity=<-1, -1>
+position=<-2,  3> velocity=< 1,  0>
+position=<-4,  3> velocity=< 2,  0>
+position=<10, -3> velocity=<-1,  1>
+position=< 5, 11> velocity=< 1, -2>
+position=< 4,  7> velocity=< 0, -1>
+position=< 8, -2> velocity=< 0,  1>
+position=<15,  0> velocity=<-2,  0>
+position=< 1,  6> velocity=< 1,  0>
+position=< 8,  9> velocity=< 0, -1>
+position=< 3,  3> velocity=<-1,  1>
+position=< 0,  5> velocity=< 0, -1>
+position=<-2,  2> velocity=< 2,  0>
+position=< 5, -2> velocity=< 1,  2>
+position=< 1,  4> velocity=< 2,  1>
+position=<-2,  7> velocity=< 2, -2>
+position=< 3,  6> velocity=<-1, -1>
+position=< 5,  0> velocity=< 1,  0>
+position=<-6,  0> velocity=< 2,  0>
+position=< 5,  9> velocity=< 1, -2>
+position=<14,  7> velocity=<-2,  0>
+position=<-3,  6> velocity=< 2, -1>";
+
+        #endregion
+
+        [Fact]
+        public void CanParse()
+        {
+            var results = new Day10(_testInput).Stars;
+            
+            var expected = new Star
+            {
+                X=6, Y = 10, VelocityY = -1, VelocityX = -2
+            };
+            Assert.Equal(expected, results[3]);
+        }
+
+        [Fact]
+        public void CanRunAOneSecondStep()
+        {
+            var day10 = new Day10(_testInput);
+            day10.MoveAll();
+
+            var expected = new Star
+            {
+                X = 4,
+                Y = 9,
+                VelocityY = -1,
+                VelocityX = -2
+            };
+            Assert.Equal(expected, day10.Stars[3]);
+        }
+
+        [Fact]
+        public void CanRunThreeSteps()
+        {
+            var day10 = new Day10(_testInput);
+            day10.MoveAll(3);
+
+            var expected = new Star
+            {
+                X = 0,
+                Y = 7,
+                VelocityY = -1,
+                VelocityX = -2
+            };
+            Assert.Equal(expected, day10.Stars[3]);
+        }
+
+        [Fact]
+        public void CanFindSecondsToConvergenceForExample()
+        {
+            var day10 = new Day10(_testInput);
+            var result = day10.FindNumberOfSecondBeforeMaximumConvergence();
+
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void CanRenderExample()
+        {
+            var day10 = new Day10(_testInput);
+
+            day10.RenderToConsoleOutput(3, _output);
+
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void CanFindSecondsToConvergenceForQ1()
+        {
+            var day10 = new Day10(Inputs.Day10);
+            var result = day10.FindNumberOfSecondBeforeMaximumConvergence();
+
+            Assert.Equal(10613, result);
+        }
+
+        [Fact]
+        public void RendersForStepsAroundConvergenceTime()
+        {
+            var maxConvergenceSecond = 10613;
+
+            for (int i = maxConvergenceSecond -2 ; i <= maxConvergenceSecond + 2; i++)
+            {
+                _output.WriteLine("---- " + i + " -------------------------------------------------------------------");
+                var day10 = new Day10(Inputs.Day10);
+
+                day10.RenderToConsoleOutput(i, _output);
+
+                _output.WriteLine("----------------------------------------------------------------------------------");
+            }
+
+            //NOTE: from visual inspection from the output we can find that 10612 gives out the answer (for this input)
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void CanRenderQ1()
+        {
+            var day10 = new Day10(Inputs.Day10);
+
+            day10.RenderToConsoleOutput(10612, _output);
+
+            Assert.True(true);
+        }
+    }
+}
---
+++ AoC2018/Day10.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace AoC2018
+{
+    public class Day10
+    {
+        public List<Star> Stars { get; }
+
+        public Day10(string input)
+        {
+            Stars = Parse(input);
+        }
+
+        private static List<Star> Parse(string input)
+        {
+            var lines = input.Split(new[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+
+            var stars = new List<Star>();
+            foreach (var line in lines)
+            {
+                var parts = line.Split(new[] {'>'}, StringSplitOptions.RemoveEmptyEntries);
+                var positionParts = parts[0].Split(new[] { '=', '<', ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                var velocityParts = parts[1].Split(new[] { '=', '<', ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                stars.Add(new Star
+                {
+                    X = int.Parse(positionParts[1]),
+                    Y = int.Parse(positionParts[2]),
+                    VelocityX = int.Parse(velocityParts[1]),
+                    VelocityY = int.Parse(velocityParts[2]),
+                });
+            }
+
+            return stars;
+        }
+
+        public void MoveAll(int seconds = 1)
+        {
+            for (int i = 0; i < seconds; i++)
+            {
+                foreach (var star in Stars)
+                {
+                    star.Move();
+                }
+            }
+        }
+
+        public double GetTotalDistanceToOrigin()
+        {
+            var distance = 0d;
+            foreach (var star in Stars)
+            {
+                var xsq = (star.X * (double)star.X);
+                var ysq = (star.Y * (double)star.Y);
+                var s = xsq + ysq;
+                var sqrt = Math.Sqrt(s);
+                distance += sqrt;
+            }
+
+            return distance;
+        }
+
+        public int FindNumberOfSecondBeforeMaximumConvergence()
+        {
+            var currentDistanceToOrigin = double.MaxValue;
+            var second = 0;
+
+            while(true)
+            {
+                MoveAll();
+                var newDistance = GetTotalDistanceToOrigin();
+
+                if (newDistance < currentDistanceToOrigin)
+                {
+                    second++;
+                    currentDistanceToOrigin = newDistance;
+                }
+                else
+                {
+                    return second;
+                }
+            }
+        }
+
+        public void RenderToConsoleOutput(int second, ITestOutputHelper output)
+        {
+            MoveAll(second);
+
+            var minX = Stars.Min(s => s.X);
+            var minY = Stars.Min(s => s.Y);
+
+            foreach (var star in Stars)
+            {
+                star.X -= minX - 2;
+                star.Y -= minY - 2;
+            }
+
+            var maxX = Stars.Max(s => s.X);
+            var maxY = Stars.Max(s => s.Y);
+
+            for (int y = 0; y < maxY +2; y++)
+            {
+                var line = new StringBuilder();
+                for (int x = 0; x < maxX + 10; x++)
+                {
+                    if (Stars.Exists(s => s.X == x && s.Y == y))
+                    {
+                        line.Append('#');
+                    }
+                    else
+                    {
+                        line.Append(' ');
+                    }
+                }
+
+                output.WriteLine(line.ToString());
+            }
+
+        }
+    }
+
+    public class Star
+    {
+        protected bool Equals(Star other)
+        {
+            return X == other.X && Y == other.Y && VelocityX == other.VelocityX && VelocityY == other.VelocityY;
+        }
+
+        public void Move()
+        {
+            X += VelocityX;
+            Y += VelocityY;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Star) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = X;
+                hashCode = (hashCode * 397) ^ Y;
+                hashCode = (hashCode * 397) ^ VelocityX;
+                hashCode = (hashCode * 397) ^ VelocityY;
+                return hashCode;
+            }
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public int VelocityX { get; set; }
+        public int VelocityY { get; set; }
+    }
+}
