a735a7aa7c389c85affc9a943e93c88e1a6715e7
Mike <micheld@gmail.com>
2018-12-09 20:34:52 -0500

    day 9

---
+++ AoC2018.tests/Da9Tests.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace AoC2018.tests
+{
+    public class Da9Tests
+    {
+        [Fact]
+        public void CanSimulateGameOfZero()
+        {
+            var day = new Day9(1,0);
+
+            Assert.Equal(new []{0}, day.Marbles.Select(x=>x).ToArray());
+        }
+
+        [Fact]
+        public void CanSimulateGameOfOne()
+        {
+            var day = new Day9(1, 1);
+
+            Assert.Equal(new [] { 0, 1 }, day.Marbles.Select(x => x).ToArray());
+        }
+
+        [Fact]
+        public void CanSimulateGameOfTwo()
+        {
+            var day = new Day9(1, 2);
+
+            Assert.Equal(new [] { 0, 2, 1 }, day.Marbles.Select(x => x).ToArray());
+        }
+
+        [Fact]
+        public void CanSimulateGameOfFive()
+        {
+            var day = new Day9(1, 5);
+
+            Assert.Equal(new [] { 0, 4, 2, 5, 1, 3 }, day.Marbles.Select(x => x).ToArray());
+        }
+
+        [Fact]
+        public void CanSimulateGameOf22()
+        {
+            var day = new Day9(1, 22);
+
+            Assert.Equal(new [] { 0, 16, 8, 17, 4, 18, 9, 19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7 ,15 }, day.Marbles.Select(x => x).ToArray());
+        }
+
+        [Fact]
+        public void CanSimulateGameOf23()
+        {
+            var day = new Day9(1, 23);
+
+            Assert.Equal(new [] { 0, 16, 8, 17, 4, 18, 19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7, 15 }, day.Marbles.Select(x => x).ToArray());
+        }
+
+        [Fact]
+        public void CanSimulateGameOf25()
+        {
+            var day = new Day9(1, 25);
+
+            Assert.Equal(new [] { 0, 16, 8, 17, 4, 18, 19, 2, 24, 20, 25, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7, 15 }, day.Marbles.Select(x => x).ToArray());
+        }
+
+        [Fact]
+        public void CanGetScoreFOrGameOf25()
+        {
+            var day = new Day9(9, 25);
+
+            var result = day.GetMaxScore();
+
+            Assert.Equal(32, result);
+        }
+
+        [Fact]
+        public void Ex2()
+        {
+            var day = new Day9(10, 1618);
+
+            var result = day.GetMaxScore();
+
+            Assert.Equal(8317, result);
+        }
+
+
+        [Fact]
+        public void Ex3()
+        {
+            var day = new Day9(13, 7999);
+
+            var result = day.GetMaxScore();
+
+            Assert.Equal(146373, result);
+        }
+
+
+        [Fact]
+        public void Ex4()
+        {
+            var day = new Day9(17, 1104);
+
+            var result = day.GetMaxScore();
+
+            Assert.Equal(2764, result);
+        }
+
+
+        [Fact]
+        public void Ex5()
+        {
+            var day = new Day9(21, 6111);
+
+            var result = day.GetMaxScore();
+
+            Assert.Equal(54718, result);
+        }
+
+        [Fact]
+        public void Ex6()
+        {
+            var day = new Day9(10, 1618);
+
+            var result = day.GetMaxScore();
+
+            Assert.Equal(8317, result);
+        }
+
+        [Fact]
+        public void Q1()
+        {
+            var day = new Day9(479, 71035);
+
+            var result = day.GetMaxScore();
+
+            Assert.Equal(367634, result);
+        }
+
+        [Fact]
+        public void Q2()
+        {
+            var day = new Day9(479, 7103500);
+   
+            var result = day.GetMaxScore();
+
+            Assert.Equal(3020072891, result);
+        }
+    }
+}
---
+++ AoC2018/Day9.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2018
+{
+    public class Day9
+    {
+        private readonly int _playerCount;
+        private readonly int _lastMarbleValue;
+        private const int ScoreTrigger = 23;
+
+        public LinkedList<int> Marbles { get; }
+        public List<Score> Scores { get; }
+
+        public Day9(int playerCount, int lastMarbleValue)
+        {
+            _playerCount = playerCount;
+            _lastMarbleValue = lastMarbleValue;
+ 
+            Marbles = new LinkedList<int>();
+            Scores = new List<Score>();
+
+            Simulate();
+        }
+
+        private void Simulate()
+        {
+            Marbles.AddFirst(0);
+            
+            if (_lastMarbleValue >= 1)
+            {
+                Marbles.AddLast(1);
+            }
+
+            var currentNode = Marbles.Last;
+
+            for (var newMarbleValue = 2; newMarbleValue <= _lastMarbleValue; newMarbleValue++)
+            {
+                if (newMarbleValue % ScoreTrigger == 0)
+                {
+                    var nodeToRemove = MoveCcw(currentNode, 7);
+
+                    currentNode = nodeToRemove.Next;
+                    Marbles.Remove(nodeToRemove);
+
+                    Scores.Add(new Score {Player = newMarbleValue % _playerCount, Value = newMarbleValue + nodeToRemove.Value});
+                }
+                else
+                {
+                    var targetNode = MoveCw(currentNode, 2);
+
+                    if (targetNode == Marbles.First)
+                    {
+                        currentNode = Marbles.AddLast(newMarbleValue);
+                    }
+                    else
+                    {
+                        currentNode = Marbles.AddBefore(targetNode, newMarbleValue);
+                    }
+                }
+            }
+        }
+
+        private LinkedListNode<int> MoveCw(LinkedListNode<int> currentNode, int steps)
+        {
+            var newNode = currentNode;
+            for (var i = 0; i < steps; i++)
+            {
+                newNode = newNode.Next ?? Marbles.First;
+            }
+
+            return newNode;
+        }
+
+        private LinkedListNode<int> MoveCcw(LinkedListNode<int> currentNode, int steps)
+        {
+            var newNode = currentNode;
+            for (var i = 0; i < steps; i++)
+            {
+                newNode = newNode.Previous ?? Marbles.Last;
+            }
+
+            return newNode;
+        }
+
+        public long GetMaxScore()
+        {
+            var maxScore = Scores.GroupBy(s => s.Player).Select(grp => new {id = grp.Key, totalValue = grp.Sum(s => s.Value)})
+                .OrderByDescending(p => p.totalValue).Select(p => p.totalValue).First();
+
+            return maxScore;
+        }
+
+        public class Score
+        {
+            public int Player { get; set; }
+            public long Value { get; set; }
+        }
+    }
+}
