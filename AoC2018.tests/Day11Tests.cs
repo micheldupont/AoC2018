c3c1d000392a298cb34286b805da71c485644041
Michel Dupont <micheld@gmail.com>
2018-12-11 15:49:53 -0500

    day 11

---
+++ AoC2018.tests/Day11Tests.cs
@@ -0,0 +1,136 @@
+﻿using Xunit;
+
+namespace AoC2018.tests
+{
+	public class Day11Tests
+	{
+		[Fact]
+		public void CanCalculatePowerLevels()
+		{
+			var cellA = new Day11(8).BuildCell(3, 5);
+			var cellB = new Day11(57).BuildCell(122, 79);
+			var cellC = new Day11(39).BuildCell(217, 196);
+			var cellD = new Day11(71).BuildCell(101, 153);
+
+			Assert.Equal(4, cellA.PowerLevel);
+			Assert.Equal(-5, cellB.PowerLevel);
+			Assert.Equal(0, cellC.PowerLevel);
+			Assert.Equal(4, cellD.PowerLevel);
+		}
+
+		[Fact]
+		public void CanFindExampleBestSquare()
+		{
+			var day11 = new Day11(18);
+
+			var result = day11.FindBestSquare();
+
+			Assert.Equal(29, result.TotalPower);
+			Assert.Equal(33, result.Corner.X);
+			Assert.Equal(45, result.Corner.Y);
+		}
+
+		[Fact]
+		public void CanFindExample2BestSquare()
+		{
+			var day11 = new Day11(42);
+
+			var result = day11.FindBestSquare();
+
+			Assert.Equal(30, result.TotalPower);
+			Assert.Equal(21, result.Corner.X);
+			Assert.Equal(61, result.Corner.Y);
+		}
+
+		[Fact]
+		public void Q1()
+		{
+			var day11 = new Day11(5093);
+
+			var result = day11.FindBestSquare();
+
+			Assert.Equal(31, result.TotalPower);
+			Assert.Equal(243, result.Corner.X);
+			Assert.Equal(49, result.Corner.Y);
+		}
+
+		[Fact]
+		public void CanGenerateSquaresOfSizeOne()
+		{
+			var day11 = new Day11(5093);
+
+			var result = day11.GenerateSquares(1);
+
+			Assert.Equal(90000, result.Count);
+		}
+
+		[Fact]
+		public void CanGenerateSquaresOfSizeTwo()
+		{
+			var day11 = new Day11(5093);
+
+			var result = day11.GenerateSquares(2);
+
+			Assert.Equal(89401, result.Count);
+		}
+
+		[Fact]
+		public void CanGenerateSquaresOfSize300()
+		{
+			var day11 = new Day11(5093);
+
+			var result = day11.GenerateSquares(300);
+
+			Assert.Equal(1, result.Count);
+		}
+
+		[Fact]
+		public void CanGenerateSquaresOfSize299()
+		{
+			var day11 = new Day11(5093);
+
+			var result = day11.GenerateSquares(299);
+
+			Assert.Equal(4, result.Count);
+		}
+
+		[Fact]
+		public void CanFindExample3BestSquare()
+		{
+			var day11 = new Day11(18);
+
+			var result = day11.FindBestSquareOfAnySize();
+
+			Assert.Equal(113, result.TotalPower);
+			Assert.Equal(90, result.Corner.X);
+			Assert.Equal(269, result.Corner.Y);
+			Assert.Equal(16, result.Size);
+		}
+
+		[Fact]
+		public void CanFindExample4BestSquare()
+		{
+			var day11 = new Day11(42);
+
+			var result = day11.FindBestSquareOfAnySize();
+
+			Assert.Equal(119, result.TotalPower);
+			Assert.Equal(232, result.Corner.X);
+			Assert.Equal(251, result.Corner.Y);
+			Assert.Equal(12, result.Size);
+		}
+
+		[Fact]
+		public void Q2()
+		{
+			var day11 = new Day11(5093);
+
+			var result = day11.FindBestSquareOfAnySize();
+
+			Assert.Equal(84, result.TotalPower);
+			Assert.Equal(285, result.Corner.X);
+			Assert.Equal(169, result.Corner.Y);
+			Assert.Equal(15, result.Size);
+		}
+	}
+}
---
+++ AoC2018/Day11.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2018
+{
+	public class Day11
+	{
+		private readonly int _serialNumber;
+		private readonly int _dimension = 300;
+		private readonly Cell[] _grid;
+
+		public Day11(int serialNumber)
+		{
+			_serialNumber = serialNumber;
+			_grid = GenerateGrid();
+		}
+
+		public Cell BuildCell(int x, int y)
+		{
+			return new Cell(x, y, _serialNumber);
+		}
+
+		public Square FindBestSquare()
+		{
+			var squares = GenerateSquares();
+			var best = squares.OrderByDescending(s => s.TotalPower).First();
+			return best;
+		}
+
+		public Square FindBestSquareOfAnySize()
+		{
+			Square bestSquareOverall = null;
+
+			var isNotGettingAnyBetterCount = 0;
+			for (int currentSize = 1; currentSize <= _dimension; currentSize++)
+			{
+				var squares = GenerateSquares(currentSize);
+				var bestForCurrentSize = squares.OrderByDescending(s => s.TotalPower).First();
+
+				if (bestSquareOverall == null || bestForCurrentSize.TotalPower > bestSquareOverall.TotalPower)
+				{
+					bestSquareOverall = bestForCurrentSize;
+					isNotGettingAnyBetterCount = 0;
+				}
+				else
+				{
+					isNotGettingAnyBetterCount++;
+				}
+
+				if (isNotGettingAnyBetterCount == 10)
+				{
+					return bestSquareOverall;
+				}
+			}
+
+			return bestSquareOverall;
+		}
+
+		public List<Square> GenerateSquares(int size = 3)
+		{
+			if (size % 2 == 0)
+			{
+				//even sized squares
+				var list = new List<Square>();
+
+				var padding = size;
+
+				for (int y = 0; y <= _dimension - padding; y++)
+				{
+					for (int x = 0; x <= _dimension - padding; x++)
+					{
+						var square = BuildEvenSizeSquareAroundCell(x, y, size);
+						list.Add(square);
+					}
+				}
+
+				return list;
+			}
+			else
+			{
+				//odd sized squares
+				var list = new List<Square>();
+
+				var padding = (size -1) / 2;
+
+				for (int y = padding; y < _dimension - padding; y++)
+				{
+					for (int x = padding; x < _dimension - padding; x++)
+					{
+						var square = BuildOddSizeSquareAroundCell(x, y, size);
+						list.Add(square);
+					}
+				}
+
+				return list;
+			}
+		}
+
+		private Square BuildEvenSizeSquareAroundCell(int x, int y, int size)
+		{
+			var topLeft = _grid[(y * _dimension) + x];
+
+			var totalPower = 0;
+			for (int currentY = y; currentY < y + size; currentY++)
+			{
+				for (int currentX = x; currentX < x + size; currentX++)
+				{
+					var currentCell = _grid[(currentY * _dimension) + currentX];
+					totalPower += currentCell.PowerLevel;
+				}
+			}
+
+			return new Square(topLeft, size, totalPower);
+		}
+
+		private Square BuildOddSizeSquareAroundCell(int x, int y, int size)
+		{
+			var padding = (size - 1) / 2;
+
+			var topLeft = _grid[((y - padding) * _dimension) + (x - padding)];
+
+			var totalPower = 0;
+			for (int currentY = y - padding; currentY <= y + padding; currentY++)
+			{
+				for (int currentX = x - padding; currentX <= x + padding; currentX++)
+				{
+					var currentCell = _grid[(currentY * _dimension) + currentX];
+					totalPower += currentCell.PowerLevel;
+				}
+			}
+
+			return new Square(topLeft, size, totalPower);
+		}
+
+		private Cell[] GenerateGrid()
+		{
+			var cells = new Cell[_dimension * _dimension];
+			for (int y = 0; y < _dimension; y++)
+			{
+				for (int x = 0; x < _dimension; x++)
+				{
+					var c = new Cell(x+1, y+1, _serialNumber);
+
+					cells[(y * _dimension) + x] = c;
+				}
+			}
+
+			return cells;
+		}
+
+		public class Cell
+		{
+			public int X { get; }
+			public int Y { get; }
+			public readonly int PowerLevel;
+
+			public Cell(int x, int y, int serialNumber)
+			{
+				X = x;
+				Y = y;
+
+				var rackId = X + 10;
+				PowerLevel = rackId * Y;
+				PowerLevel = PowerLevel + serialNumber;
+				PowerLevel = PowerLevel * rackId;
+				PowerLevel = GetThirdDigitOfNumber(PowerLevel);
+				PowerLevel = PowerLevel - 5;
+			}
+
+			private int GetThirdDigitOfNumber(int number)
+			{
+				if (number < 100)
+				{
+					throw new Exception("unexpected value: " + number);
+				}
+
+				var downToThreeDigit = number % 1000;
+				var third = downToThreeDigit / 100;
+
+				return third;
+			}
+		}
+
+		public class Square
+		{
+			public int Size { get; }
+			public int TotalPower { get;}
+			public Cell Corner { get; }
+
+			public Square(Cell corner, int size, int totalPower = 0)
+			{
+				Size = size;
+				Corner = corner;
+				TotalPower = totalPower;
+			}
+		}
+	}
+}
