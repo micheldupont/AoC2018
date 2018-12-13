5d8b0ed3d56b636dab18c3d5d3341b306e0e7a09
Michel Dupont <micheld@gmail.com>
2018-12-13 18:26:30 -0500

    day 13

---
+++ AoC2018.tests/Day13Tests.cs
@@ -0,0 +1,176 @@
+﻿using Xunit;
+
+namespace AoC2018.tests
+{
+	public class Day13Tests
+	{
+		private string _testInput = @"
+/->-\        
+|   |  /----\
+| /-+--+-\  |
+| | |  | v  |
+\-+-/  \-+--/
+  \------/   
+";
+
+		[Fact]
+		public void CanParseMap()
+		{
+			var day13 = new Day13(_testInput);
+
+			Assert.Equal('/', day13.Map[0,0]);
+			Assert.Equal('-', day13.Map[1, 0]);
+			Assert.Equal('|', day13.Map[0, 2]);
+			Assert.Equal('+', day13.Map[4, 2]);
+			Assert.Equal(13, day13.Map.GetLength(0));
+			Assert.Equal(6, day13.Map.GetLength(1));
+			Assert.Equal(' ', day13.Map[12, 5]);
+		}
+
+		[Fact]
+		public void CanDetectCarts()
+		{
+			var day13 = new Day13(_testInput);
+
+			Assert.Equal('-', day13.Map[2, 0]);
+			Assert.Equal('|', day13.Map[9, 3]);
+			Assert.Equal(2, day13.Carts.Count);		
+		}
+
+		[Fact]
+		public void CanParseCarts()
+		{
+			var day13 = new Day13(_testInput);
+
+			Assert.Equal(2, day13.Carts.Count);
+			Assert.Equal(2, day13.Carts[0].X);
+			Assert.Equal(0, day13.Carts[0].Y);
+			Assert.Equal('-', day13.Carts[0].Path);
+			Assert.Equal(Day13.Headings.Right, day13.Carts[0].Heading);
+
+			Assert.Equal(9, day13.Carts[1].X);
+			Assert.Equal(3, day13.Carts[1].Y);
+			Assert.Equal('|', day13.Carts[1].Path);
+			Assert.Equal(Day13.Headings.Down, day13.Carts[1].Heading);
+		}
+
+		[Fact]
+		public void CartCanMove()
+		{
+			var day13 = new Day13(_testInput);
+			var cart = day13.Carts[0];
+
+			cart.Move();
+
+			Assert.Equal(3, cart.X);
+			Assert.Equal(0, cart.Y);
+			Assert.Equal('-', cart.Path);
+			Assert.Equal(Day13.Headings.Right, cart.Heading);
+
+			cart.Move();
+
+			Assert.Equal(4, cart.X);
+			Assert.Equal(0, cart.Y);
+			Assert.Equal('\\', cart.Path);
+			Assert.Equal(Day13.Headings.Down, cart.Heading);
+
+			cart.Move();
+
+			Assert.Equal(4, cart.X);
+			Assert.Equal(1, cart.Y);
+			Assert.Equal('|', cart.Path);
+			Assert.Equal(Day13.Headings.Down, cart.Heading);
+		}
+
+		[Fact]
+		public void CartCanMoveThroughIntersections()
+		{
+			var day13 = new Day13(_testInput);
+			var cart = day13.Carts[0];
+
+			cart.Move();
+			cart.Move();
+			cart.Move();
+			cart.Move(); //x here, go left
+			cart.Move();
+
+			Assert.Equal(5, cart.X);
+			Assert.Equal(2, cart.Y);
+			Assert.Equal('-', cart.Path);
+			Assert.Equal(Day13.Headings.Right, cart.Heading);
+
+			cart.Move();
+			cart.Move(); //x here, go straight
+			cart.Move();
+
+			Assert.Equal(8, cart.X);
+			Assert.Equal(2, cart.Y);
+			Assert.Equal('-', cart.Path);
+			Assert.Equal(Day13.Headings.Right, cart.Heading);
+
+			cart.Move();
+			cart.Move();
+			cart.Move(); //x here, go right
+			cart.Move();
+
+			Assert.Equal(8, cart.X);
+			Assert.Equal(4, cart.Y);
+			Assert.Equal('-', cart.Path);
+			Assert.Equal(Day13.Headings.Left, cart.Heading);
+		}
+
+		[Fact]
+		public void CanDetectCrashes()
+		{
+			var day13 = new Day13(_testInput);
+
+			var crash = day13.SimUntilCrash();
+
+			Assert.Equal(7, crash.X);
+			Assert.Equal(3, crash.Y);
+		}
+
+		[Fact]
+		public void Q1()
+		{
+			var day13 = new Day13(Inputs.Day13);
+
+			var crash = day13.SimUntilCrash();
+
+			Assert.Equal(103, crash.X);
+			Assert.Equal(85, crash.Y);
+		}
+
+		private string _testInputB = @"
+/>-<\  
+|   |  
+| /<+-\
+| | | v
+\>+</ |
+  |   ^
+  \<->/
+";
+
+		[Fact]
+		public void CanSimUntilOneCartRemains()
+		{
+			var day13 = new Day13(_testInputB);
+
+			var lastCart = day13.SimToLastCart();
+
+			Assert.Equal(6, lastCart.X);
+			Assert.Equal(4, lastCart.Y);
+		}
+
+		[Fact]
+		public void Q2()
+		{
+			var day13 = new Day13(Inputs.Day13);
+
+			var lastCart = day13.SimToLastCart();
+
+			Assert.Equal(88, lastCart.X);
+			Assert.Equal(64, lastCart.Y);
+		}
+	}
+}
---
+++ AoC2018/Day13.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+
+namespace AoC2018
+{
+	public class Day13
+	{
+		public char[,] Map { get; private set; }
+		public List<Cart> Carts { get; }
+
+		private readonly char[] _cartChars = { '<', '^', '>', 'v' };
+
+		public Day13(string testInput)
+		{
+			Carts = new List<Cart>();
+			Parse(testInput);
+		}
+
+		private void Parse(string testInput)
+		{
+			var lines = testInput.Split(new[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+			var xSize = lines[0].Length;
+			var ySize = lines.Length;
+
+			Map = new char[xSize,ySize];
+
+			for (var y = 0; y < ySize; y++)
+			{
+				var currentLine = lines[y].ToCharArray();
+				for (var x = 0; x < xSize; x++)
+				{
+					var currentChar = currentLine[x];
+
+					if (_cartChars.Contains(currentChar))
+					{
+						if (currentChar == '<' || currentChar == '>')
+						{
+							Map[x, y] = '-';
+							Carts.Add(new Cart(x, y, '-', currentChar == '<' ? Headings.Left : Headings.Right, Map));
+						}
+						else
+						{
+							Map[x, y] = '|';
+							Carts.Add(new Cart(x, y, '|', currentChar == '^' ? Headings.Up : Headings.Down, Map));
+						}
+					}
+					else
+					{
+						Map[x, y] = currentChar;
+					}
+				}
+			}
+		}
+
+		public CrashInfo SimUntilCrash()
+		{
+			var sortedCarts = Carts.OrderBy(c => c.Y).ThenBy(c => c.X).ToList();
+
+			while (true)
+			{
+				foreach (var cart in sortedCarts)
+				{
+					cart.Move();
+					if (sortedCarts.Find(c => !c.Equals(cart) && c.X == cart.X && c.Y == cart.Y) != null)
+					{
+						return new CrashInfo(cart.X, cart.Y);
+					}
+				}
+			}
+		}
+
+		public Cart SimToLastCart()
+		{
+			var sortedCarts = Carts.OrderBy(c => c.Y).ThenBy(c => c.X).ToList();
+
+			while (true)
+			{
+				var crashedCart = new List<Cart>();
+
+				foreach (var cart in sortedCarts)
+				{
+					cart.Move();
+					var cart1 = cart;
+					var cartCrashed = sortedCarts.Find(c => !c.Equals(cart1) && c.X == cart1.X && c.Y == cart1.Y);
+					if (cartCrashed != null)
+					{
+						//crashed!
+						crashedCart.Add(cart);
+						crashedCart.Add(cartCrashed);
+					}
+				}
+
+				foreach (var cart in crashedCart.Distinct())
+				{
+					sortedCarts.Remove(cart);
+				}
+
+				if (sortedCarts.Count == 1)
+				{
+					return sortedCarts.First();
+				}
+
+				if (sortedCarts.Count == 0)
+				{
+					throw new Exception("All dead!");
+				}
+			}
+		}
+
+		public enum Headings
+		{
+			Up,
+			Down,
+			Left,
+			Right
+		}
+
+		public enum Decisions
+		{
+			GoLeft,
+			GoStraight,
+			GoRight
+		}
+
+		public class Cart
+		{
+			private readonly char[,] _map;
+			public int X { get; set; }
+			public int Y { get; set; }
+			public char Path { get; set; }
+			public Headings Heading { get; set; }
+
+			private readonly Decisions[] _decisions = { Decisions.GoLeft, Decisions.GoStraight, Decisions.GoRight };
+			private int _nextDecisionIndex;
+		
+			public Cart(int x, int y, char path, Headings heading, char[,] map)
+			{
+				_map = map;
+				X = x;
+				Y = y;
+				Path = path;
+				Heading = heading;
+				_nextDecisionIndex = 0;
+			}
+
+			public void Move()
+			{
+				UpdatePosition();
+				UpdateHeading();
+			}
+
+			private void UpdatePosition()
+			{
+				switch (Path)
+				{
+					case '-':
+					case '|':
+					case '/':
+					case '\\':
+					case '+':
+						switch (Heading)
+						{
+							case Headings.Left:
+								X--;
+								break;
+							case Headings.Right:
+								X++;
+								break;
+							case Headings.Up:
+								Y--;
+								break;
+							case Headings.Down:
+								Y++;
+								break;
+						}
+
+						break;
+					default:
+						throw new Exception("Invalid path! : " + Path);
+				}
+
+				Path = _map[X, Y];
+			}
+
+			private void UpdateHeading()
+			{
+				switch (Path)
+				{
+					case '-':
+						break;
+					case '|':
+						break;
+					case '/':
+						switch (Heading)
+						{
+							case Headings.Left:
+								Heading = Headings.Down;
+								break;
+							case Headings.Up:
+								Heading = Headings.Right;
+								break;
+							case Headings.Right:
+								Heading = Headings.Up;
+								break;
+							case Headings.Down:
+								Heading = Headings.Left;
+								break;
+						}
+
+						break;
+					case '\\':
+						switch (Heading)
+						{
+							case Headings.Left:
+								Heading = Headings.Up;
+								break;
+							case Headings.Up:
+								Heading = Headings.Left;
+								break;
+							case Headings.Right:
+								Heading = Headings.Down;
+								break;
+							case Headings.Down:
+								Heading = Headings.Right;
+								break;
+						}
+
+						break;
+					case '+':
+						HandleIntersection();
+						break;
+					default:
+						throw new Exception("Invalid path! : " + Path);
+				}
+			}
+
+			private void HandleIntersection()
+			{
+				switch (_decisions[_nextDecisionIndex])
+				{
+					case Decisions.GoLeft:
+						switch (Heading)
+						{
+							case Headings.Left:
+								Heading = Headings.Down;
+								break;
+							case Headings.Up:
+								Heading = Headings.Left;
+								break;
+							case Headings.Right:
+								Heading = Headings.Up;
+								break;
+							case Headings.Down:
+								Heading = Headings.Right;
+								break;
+						}
+
+						break;
+					case Decisions.GoStraight:
+						break;
+					case Decisions.GoRight:
+						switch (Heading)
+						{
+							case Headings.Left:
+								Heading = Headings.Up;
+								break;
+							case Headings.Up:
+								Heading = Headings.Right;
+								break;
+							case Headings.Right:
+								Heading = Headings.Down;
+								break;
+							case Headings.Down:
+								Heading = Headings.Left;
+								break;
+						}
+
+						break;
+				}
+
+				_nextDecisionIndex++;
+				_nextDecisionIndex = _nextDecisionIndex % _decisions.Length;
+			}
+		}
+
+		public class CrashInfo
+		{
+			public int X { get; }
+			public int Y { get; }
+
+			public CrashInfo(int x, int y)
+			{
+				this.X = x;
+				this.Y = y;
+			}
+		}
+	}
+}
