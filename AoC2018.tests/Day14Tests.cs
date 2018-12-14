64f69a1eddd755218a08b30b3c76b95d3790cfa9
Michel Dupont <micheld@gmail.com>
2018-12-14 13:45:16 -0500

    day 14

---
+++ AoC2018.tests/Day14Tests.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Xunit;
+
+namespace AoC2018.tests
+{
+	public class Day14Tests
+	{
+		[Fact]
+		public void CanDoInit()
+		{
+			var day = new Day14(2, new[] {3, 7});
+
+			Assert.Equal(0, day.ElvesCurrentRecipeIndexes[0]);
+			Assert.Equal(1, day.ElvesCurrentRecipeIndexes[1]);
+			Assert.Equal(3, day.RecipeScores[0]);
+			Assert.Equal(7, day.RecipeScores[1]);
+		}
+
+		[Fact]
+		public void CanDoFirstGeneration()
+		{
+			var day = new Day14(2, new[] {3, 7});
+
+			day.CreateNewRecipes();
+
+			Assert.Equal(4, day.RecipeScores.Count);
+			Assert.Equal(1, day.RecipeScores[2]);
+			Assert.Equal(0, day.RecipeScores[3]);
+		}
+
+		[Fact]
+		public void ElvesCanPickNewRecipes()
+		{
+			var day = new Day14(2, new[] {3, 7});
+
+			day.CreateNewRecipes();
+
+			Assert.Equal(0, day.ElvesCurrentRecipeIndexes[0]);
+			Assert.Equal(1, day.ElvesCurrentRecipeIndexes[1]);
+
+			day.CreateNewRecipes();
+
+			Assert.Equal(4, day.ElvesCurrentRecipeIndexes[0]);
+			Assert.Equal(3, day.ElvesCurrentRecipeIndexes[1]);
+		}
+
+		[Fact]
+		public void CanDoTenGenerations()
+		{
+			var day = new Day14(2, new[] {3, 7});
+
+			day.CreateNewRecipes();
+			day.CreateNewRecipes();
+			day.CreateNewRecipes();
+			day.CreateNewRecipes();
+			day.CreateNewRecipes();
+			day.CreateNewRecipes();
+			day.CreateNewRecipes();
+			day.CreateNewRecipes();
+			day.CreateNewRecipes();
+			day.CreateNewRecipes();
+
+			Assert.Equal(1, day.ElvesCurrentRecipeIndexes[0]);
+			Assert.Equal(13, day.ElvesCurrentRecipeIndexes[1]);
+		}
+
+		[Fact]
+		public void ExampleA()
+		{
+			var day = new Day14(2, new[] { 3, 7 });
+
+			var result = day.GetScoreAfterRecipeCount(9);
+
+			Assert.Equal("5158916779", result);
+		}
+
+		[Fact]
+		public void ExampleB()
+		{
+			var day = new Day14(2, new[] { 3, 7 });
+
+			var result = day.GetScoreAfterRecipeCount(5);
+
+			Assert.Equal("0124515891", result);
+		}
+
+		[Fact]
+		public void ExampleC()
+		{
+			var day = new Day14(2, new[] { 3, 7 });
+
+			var result = day.GetScoreAfterRecipeCount(18);
+
+			Assert.Equal("9251071085", result);
+		}
+
+		[Fact]
+		public void ExampleD()
+		{
+			var day = new Day14(2, new[] { 3, 7 });
+
+			var result = day.GetScoreAfterRecipeCount(2018);
+
+			Assert.Equal("5941429882", result);
+		}
+
+		[Fact]
+		public void Q1()
+		{
+			var day = new Day14(2, new[] { 3, 7 });
+
+			var result = day.GetScoreAfterRecipeCount(637061);
+
+			Assert.Equal("3138510102", result);
+		}
+
+		[Fact]
+		public void ExampleA2()
+		{
+			var day = new Day14(2, new[] { 3, 7 });
+
+			var result = day.FindRecipeCountForScore("51589");
+
+			Assert.Equal(9, result);
+		}
+
+		[Fact]
+		public void ExampleB2()
+		{
+			var day = new Day14(2, new[] { 3, 7 });
+
+			var result = day.FindRecipeCountForScore("01245");
+
+			Assert.Equal(5, result);
+		}
+
+		[Fact]
+		public void ExampleC2()
+		{
+			var day = new Day14(2, new[] { 3, 7 });
+
+			var result = day.FindRecipeCountForScore("92510");
+
+			Assert.Equal(18, result);
+		}
+		[Fact]
+		public void ExampleD2()
+		{
+			var day = new Day14(2, new[] { 3, 7 });
+
+			var result = day.FindRecipeCountForScore("59414");
+
+			Assert.Equal(2018, result);
+		}
+
+		[Fact]
+		public void Q2()
+		{
+			var day = new Day14(2, new[] { 3, 7 });
+
+			var result = day.FindRecipeCountForScore("637061");
+
+			Assert.Equal(20179081, result);
+		}
+	}
+}
---
+++ AoC2018/Day14.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AoC2018
+{
+	public class Day14
+	{
+		private const int ScoreLength = 10;
+		public List<int> RecipeScores { get; }
+		public List<int> ElvesCurrentRecipeIndexes { get; }
+
+
+		public Day14(int elfCount, int[] initialScores)
+		{
+			RecipeScores = new List<int>(initialScores);
+			ElvesCurrentRecipeIndexes = new List<int>();
+
+			for (var i = 0; i < elfCount; i++)
+			{
+				ElvesCurrentRecipeIndexes.Add(i);
+			}
+		}
+
+		public void CreateNewRecipes()
+		{
+			var combinedScore = 0;
+
+			foreach (var elfRecipeIndex in ElvesCurrentRecipeIndexes)
+			{
+				combinedScore += RecipeScores[elfRecipeIndex];
+			}
+
+			if (combinedScore >= 10)
+			{
+				var first = combinedScore / 10;
+				var second = combinedScore % 10;
+
+				RecipeScores.AddRange(new[] {first, second});
+			}
+			else
+			{
+				RecipeScores.Add(combinedScore);
+			}
+
+			for (var index = 0; index < ElvesCurrentRecipeIndexes.Count; index++)
+			{
+				var elfRecipeIndex = ElvesCurrentRecipeIndexes[index];
+
+				var currentRecipeScore = RecipeScores[elfRecipeIndex];
+				var steps = currentRecipeScore + 1;
+
+				var newIndex = elfRecipeIndex + steps;
+				var normalizedIndex = newIndex % RecipeScores.Count;
+
+				ElvesCurrentRecipeIndexes[index] = normalizedIndex;
+			}
+		}
+
+		public string GetScoreAfterRecipeCount(int recipeCount)
+		{
+			var targetRecipeCount = recipeCount + ScoreLength;
+
+			while (RecipeScores.Count < targetRecipeCount)
+			{
+				CreateNewRecipes();
+			}
+
+			var scoreItems = RecipeScores.GetRange(recipeCount, ScoreLength);
+			var score = new StringBuilder();
+			foreach (var scoreItem in scoreItems)
+			{
+				score.Append(scoreItem);
+			}
+
+			return score.ToString();
+		}
+
+		public int FindRecipeCountForScore(string input)
+		{
+			var inputAsIntArray = input.ToCharArray().Select(c => int.Parse(new string(new[] {c}))).ToArray();
+			
+			while (!InputExistsInScores(inputAsIntArray))
+			{
+				CreateNewRecipes();
+			}
+
+			var scoreBuilder = new StringBuilder(RecipeScores.Count);
+			foreach (var scoreItem in RecipeScores)
+			{
+				scoreBuilder.Append(scoreItem);
+			}
+
+			var scoresAsString = scoreBuilder.ToString();
+			var matchIndex = scoresAsString.IndexOf(input, StringComparison.Ordinal);
+			var scoreBeforeMatch = scoresAsString.Substring(0, matchIndex);
+
+			return scoreBeforeMatch.Length;
+		}
+
+		private bool InputExistsInScores(int[] inputAsIntArray)
+		{
+			var scores = RecipeScores;
+			var scoresLength = scores.Count;
+			var inputLength = inputAsIntArray.Length;
+
+			// always check at the end of the score list
+			// (padding by 2 because that's the max number of value we can add in one turn)
+			var scoreStartingIndex = scoresLength - inputLength - 2; 
+			if (scoreStartingIndex < 0)
+				scoreStartingIndex = 0;
+
+			for (var recipeIndex = scoreStartingIndex; recipeIndex < scoresLength; recipeIndex++)
+			{
+				//if there is not enough item in score left to cover the input length, no use to keep going
+				if (recipeIndex + inputLength > scoresLength - 1) continue;
+
+				//if no match here there is no use to keep going
+				if (scores[recipeIndex] != inputAsIntArray[0]) continue;
+
+				var j = recipeIndex;
+				for (var i = 0; i < inputLength; i++)
+				{
+					if (scores[j] == inputAsIntArray[i])
+					{
+						j++;
+					}
+					else
+					{
+						break;
+					}
+				}
+
+				//if we went through all inputs without break-ing, we have a match
+				if ((j - recipeIndex) == inputLength)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
